4746f31fb9e9d4459f1ee99f95517fad79375cad
Vaia Sanu <vaia.sanu@yahoo.com>
2022-04-14 02:02:32 +0700

    mui library

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,12 +1,17 @@
-import React from "react";
+import * as React from "react";
+import Stack from "@mui/material/Stack";
+import { Button } from '@mui/material';
 import { redirectToSpotify } from "../../authentication/Auth";
+import "../../components/Card.css"
 
 export default function SpotifyLogin() {
   return (
-    <div className="btn top-btn centered">
-      <button>
-        <a href={redirectToSpotify()}>Login</a>
-      </button>
+    <div  className="centered logincenter">
+      <Stack spacing={2} direction="row">
+        <Button variant="contained">
+          <a className="nodeco white-color" href={redirectToSpotify()}>Login</a>
+        </Button>
+      </Stack>
     </div>
   );
 }
